5d3d9a2d7e1e34513eb0de004447e12d3c5cc171
Nagaruru Rohankumar Reddy <150910334+RohankumarReddy@users.noreply.github.com>
2025-12-11 02:45:27 +0530

    Edited the tile and styling in the backgound

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,10 +4,9 @@ const Background = () => {
   return (
     <div className="absolute top-0 left-0 z-[2] w-full h-full">
       <div className="absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold">
-        Documents.
       </div>
       <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900">
-        Docs.
+        NoteIt.
       </h1>
     </div>
   );
